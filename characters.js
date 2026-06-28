acd628cbd088b6e4cc8f7b8bc22537cc6111d77a
Sunutarou <zidane5kei1kaka22nagashima3@gmail.com>
2026-06-28 15:58:05 +0900

    Add files via upload

---
+++ characters.js
@@ -0,0 +1,36 @@
+const characterDotMap = {
+    "ゆたかママ": "Yutaka.dot.png",
+    "聖奈": "Sena.dot.png",
+    "なお": "Nao.dot.png",
+    "こころ": "Kokoro.dot.png",
+    "ゆかりママ": "Yukari.dot.png",
+    "青リンゴ": "Ringo.dot.png",
+    "むぎ": "Mugi.dot.png",
+    "スヌタロウ": "Sunutarou.dot.png",
+    "よしみ姉さん": "Yoshimi.dot.png",
+    "くさみ": "Kusami.dot.png",
+    "青リンゴママ": "Aoringomama.png",
+    "新人スタッフ福原くん": "shinjinfukuhara.png",
+"コタニ": "Kotani.dot.png",
+"オオタニ": "Ohtani.dot.png",
+"ナカタニ": "nakatani.dot.png",
+"久美": "Kumi.jpg",
+
+};
+
+const characterEmojiMap = {
+  "ゆたかママ": "🍗",
+  "聖奈": "💍",
+  "スヌタロウ": "🐶",
+  "なお": "💄",
+  "こころ": "🍾👿",
+  "ゆかりママ": "💎",
+  "青リンゴ": "🍏",
+  "むぎ": "🍺",
+  "よしみ姉さん": "💃",
+  "くさみ": "💋",
+  "青リンゴママ": "🍏",
+  "新人スタッフ福原くん": "🔰",
+"久美": "👵"
+
+};
